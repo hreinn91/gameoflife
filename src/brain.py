795ce77534be2e1223f75b7da5a9933a909524d9
hreinn91 <hreinnj@localhost.localdomain>
2021-07-05 16:00:55 +0200

    Apparantly one init value is needed to imshow empty matirx

--- src/brain.py
+++ src/brain.py
@@ -1,18 +1,37 @@
 import numpy as np
 from random import randrange
 
+ON = 255
+OFF = 0
+
+
 class Brain:
     def __init__(self, size):
         print('Init')
         self.size = size
-        self.matrix = np.zeros([size, size]).astype('int64')
+        # self.matrix = np.random.choice([ON, OFF], size * size, p=[0.2, 0.8]).reshape(size, size)
+        self.matrix = empty_init(size)
 
     def step(self):
         print('Arrrg')
         row = randrange(self.size)
         col = randrange(self.size)
-        # self.matrix[row][col] = self.matrix[row][col] + 1
-        self.matrix[1, 1] = self.matrix[1, 1] + 10
-        self.matrix[0, 1] = self.matrix[0, 1] + 10
+        print(self.matrix[row, col])
+
+        if self.matrix[row, col] == ON:
+            self.matrix[row, col] = OFF
+        else:
+            self.matrix[row, col] = ON
+
         print(self.matrix)
         return self.matrix
+
+
+def random_init(size):
+    return np.random.choice([ON, OFF], size * size, p=[0.2, 0.8]).reshape(size, size)
+
+
+def empty_init(size):
+    np.zeros(size * size).reshape(size, size)
+    # One value needed to init animation
+    self.matrix[1, 1] = 1
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from random import randrange
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -7,6 +8,9 @@ from copy import deepcopy
 
 
 # Unix requires python3-tk
+
+ON = 255
+OFF = 0
 
 def plot_conf_style():
     # TkAgg required for display in Pycharm - render in canvas
@@ -23,22 +27,24 @@ def foo_plot():
     plt.show()
 
 
-def update(i, img, grid):
-    new_grid = grid
-    if new_grid[0, 1] == 255:
-        new_grid[0, 1] = 0
+def update(i, img, brain):
+    grid = brain.matrix.copy()
+    row = randrange(brain.size)
+    col = randrange(brain.size)
+    print(grid[row, col])
+    if grid[row, col] == ON:
+        grid[row, col] = OFF
     else:
-        new_grid[0, 1] = 255
-
-    img.set_data(new_grid)
-    # grid[:] = new_grid[:]
+        grid[row, col] = ON
+    img.set_data(grid)
+    brain.matrix = grid
     return img,
 
 
-def plot(grid):
+def plot(brain):
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid),
+    img = ax.imshow(brain.matrix, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, brain),
                                   frames=19, interval=500, save_count=50)
     plt.show()
     pass
@@ -46,7 +52,6 @@ def plot(grid):
 
 if __name__ == '__main__':
     plot_conf_style()
-    N = 5
-    vals = [255, 0]
-    grid = np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
-    plot(grid)
+    brain = Brain(50)
+    # grid = np.random.choice(vals, N * N, p=[0.2, 0.8]).reshape(N, N)
+    plot(brain)
